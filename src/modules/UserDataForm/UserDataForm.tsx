df45dfd4474657734b155c187b53403bb0873a86
Michell Ayala <starships@outlook.com>
2019-02-27 17:11:03 -0600

    refactor: move logic/state inside user-data-form

--- src/modules/UserDataForm/UserDataForm.tsx
+++ src/modules/UserDataForm/UserDataForm.tsx
@@ -1,46 +1,122 @@
 import React from "react";
-import { IProfile } from "../../models";
+import { IProfile, IUserService } from "../../models";
+import { If } from "../../modules";
 
 interface IUserDataFormProps {
-  onChangeFirstName: (e: any) => void;
-  onChangeLastName: (e: any) => void;
-  onClickSave: (e: any) => void;
-  data: IProfile;
-  isSaving: boolean;
+  userService: IUserService;
 }
 
-export const UserDataForm: React.SFC<IUserDataFormProps> = props => {
-  return (
-    <form className="row profile-form">
-      <div className="columns small-12">
-        <label>First Name</label>
-        <input
-          type="text"
-          name="firstName"
-          onChange={props.onChangeFirstName}
-          value={props.data.firstName}
-        />
-      </div>
-      <div className="columns small-12">
-        <label>Last Name</label>
-        <input
-          type="text"
-          name="lastName"
-          onChange={props.onChangeFirstName}
-          value={props.data.lastName}
-        />
-      </div>
-      <div className="columns small-12">
-        <button
-          type="button"
-          className={
-            props.isSaving ? "button expanded disabled" : "button expanded"
-          }
-          onClick={props.onClickSave}
-        >
-          Save
-        </button>
-      </div>
-    </form>
-  );
-};
+interface IUserDataFormState {
+  data: IProfile;
+  isSaving: boolean;
+  isLoading: boolean;
+}
+
+export class UserDataForm extends React.Component<
+  IUserDataFormProps,
+  IUserDataFormState
+> {
+  state = {
+    data: {
+      firstName: "",
+      lastName: ""
+    },
+    isSaving: false,
+    isLoading: true
+  };
+
+  async componentDidMount() {
+    const data = await this.props.userService.getProfileData();
+    this.setState({
+      data,
+      isLoading: false
+    });
+  }
+
+  handleChangeFirstName = ({ target: { value } }: any): void => {
+    this.setState(prevState => ({
+      data: {
+        ...prevState.data,
+        firstName: value
+      }
+    }));
+  };
+
+  handleChangeLastName = ({ target: { value } }: any): void => {
+    this.setState(prevState => ({
+      data: {
+        ...prevState.data,
+        lastName: value
+      }
+    }));
+  };
+
+  handleClickSave = async () => {
+    if (this.state.isSaving) {
+      return;
+    }
+
+    this.setState(prevState => ({
+      ...prevState,
+      isSaving: true
+    }));
+
+    const data = await this.props.userService.updateProfileData(
+      this.state.data
+    );
+
+    this.setState(prevState => ({
+      ...prevState,
+      isSaving: false,
+      data
+    }));
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <If predicate={this.state.isLoading}>
+          <p className="profile-loading">Loading</p>
+        </If>
+
+        <If predicate={!this.state.isLoading}>
+          <form className="row profile-form">
+            <div className="columns small-12">
+              <label>First Name</label>
+              <input
+                type="text"
+                name="firstName"
+                onChange={this.handleChangeFirstName}
+                value={this.state.data.firstName}
+                disabled={this.state.isSaving}
+              />
+            </div>
+            <div className="columns small-12">
+              <label>Last Name</label>
+              <input
+                type="text"
+                name="lastName"
+                onChange={this.handleChangeLastName}
+                value={this.state.data.lastName}
+                disabled={this.state.isSaving}
+              />
+            </div>
+            <div className="columns small-12">
+              <button
+                type="button"
+                className={
+                  this.state.isSaving
+                    ? "button expanded disabled"
+                    : "button expanded"
+                }
+                onClick={this.handleClickSave}
+              >
+                Save
+              </button>
+            </div>
+          </form>
+        </If>
+      </React.Fragment>
+    );
+  }
+}
--- src/routes/Profile/Profile.tsx
+++ src/routes/Profile/Profile.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { IProfile, IUserService } from "../../models";
-import { If, Navigation, UserDataForm } from "../../modules";
+import { Navigation, UserDataForm } from "../../modules";
 import { UserServiceContext } from "../../providers";
 import "./profile.css";
 
@@ -15,82 +15,15 @@ export interface IProfileState {
 }
 
 export class Profile extends React.Component<IProfileProps, IProfileState> {
-  state = {
-    isLoading: true,
-    isSaving: false,
-    profile: {
-      firstName: "",
-      lastName: ""
-    }
-  };
-
   static contextType = UserServiceContext;
-
-  async componentDidMount() {
-    const profile = await this.props.userService.getProfileData();
-    this.setState({
-      isLoading: false,
-      profile
-    });
-  }
-
-  handleFirstNameChange = ({ target: { value } }: any): void => {
-    this.setState(prevState => ({
-      profile: {
-        ...prevState.profile,
-        firstName: value
-      }
-    }));
-  };
-
-  handleLastNameChange = ({ target: { value } }: any): void => {
-    this.setState(prevState => ({
-      profile: {
-        ...prevState.profile,
-        lastName: value
-      }
-    }));
-  };
-
-  handleClickSave = async () => {
-    if (this.state.isSaving) {
-      return;
-    }
-
-    this.setState(prevState => ({
-      ...prevState,
-      isSaving: true
-    }));
-
-    const profile = await this.props.userService.updateProfileData(
-      this.state.profile
-    );
-
-    this.setState(prevState => ({
-      ...prevState,
-      isSaving: false,
-      profile
-    }));
-  };
 
   render() {
     return (
       <div>
         <Navigation title="Profile" />
-
-        <If predicate={this.state.isLoading}>
-          <p className="profile-loading">Loading</p>
-        </If>
-
-        <If predicate={!this.state.isLoading}>
-          <UserDataForm
-            data={this.state.profile}
-            isSaving={this.state.isSaving}
-            onChangeFirstName={this.handleFirstNameChange}
-            onChangeLastName={this.handleLastNameChange}
-            onClickSave={this.handleClickSave}
-          />
-        </If>
+        <UserServiceContext.Consumer>
+          {userService => <UserDataForm userService={userService} />}
+        </UserServiceContext.Consumer>
       </div>
     );
   }
